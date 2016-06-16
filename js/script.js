6d58715f7bb8e595fe9fd85cb4b87e49b90b629f
Mario Silva <noncasus@gmail.com>
2016-06-16 08:33:53 +0000

    Updated '/js/script.js' via CloudCannon

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
             debug:true,
             anchors: 'a',
             onStart: {
-                duration: 1000, // ms
+                duration: 200, // ms
                 render: function ( $container ) {
                     $container.removeClass('site-content-in').delay(1000).addClass('site-content-out');
                 }
