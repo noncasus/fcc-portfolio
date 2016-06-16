443ddc0d6a2cb81cc40c9baaf874f71663b55199
Mario Silva <noncasus@gmail.com>
2016-06-16 08:22:12 +0000

    Updated '/js/script.js' via CloudCannon

--- js/script.js
+++ js/script.js
@@ -5,13 +5,13 @@
             debug:true,
             anchors: 'a',
             onStart: {
-                duration: 1500, // ms
+                duration: 900, // ms
                 render: function ( $container ) {
-                    $container.removeClass('site-content-in').delay(500).addClass('site-content-out');
+                    $container.removeClass('site-content-in').delay(100).addClass('site-content-out');
                 }
             },
             onAfter: function( $container ) {
-                $container.removeClass('site-content-out').delay(500).addClass('site-content-in');
+                $container.removeClass('site-content-out').delay(100).addClass('site-content-in');
             }
         };
  
