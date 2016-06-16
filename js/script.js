1c7c3b018ae59f720817ae16223ffced3fde4a73
Mario Silva <noncasus@gmail.com>
2016-06-16 06:11:11 +0000

    Updated '/js/script.js' via CloudCannon

--- js/script.js
+++ js/script.js
@@ -2,46 +2,20 @@
  
     $( function() { // Ready
         'use strict';
-        var options = {
-            prefetch: true,
-            cacheLength: 2,
+        var settings = { 
+            anchors: 'a',
             onStart: {
-                duration: 2000,
-                render: function ($container) {
-                    // Add your CSS animation reversing class
-                    $container.addClass('slide-out');
-                    
-                    // Restart your animation
-                    smoothState.restartCSSAnimations();
+                duration: 20000, // ms
+                render: function ( $container ) {
+                    $container.addClass( 'slide-out' );
                 }
             },
-            onReady: {
-                duration: 0,
-                render: function ($container, $newContent) {
-                    // Remove your CSS animation reversing class
-                    $container.removeClass('slide-out');
-                    
-                    // Inject the new content
-                    $container.html($newContent);
-                    
-                    }
-                }
-        },
-        smoothState = $('#container').smoothState(options).data('smoothState');
-        // var settings = { 
-        //     anchors: 'a',
-        //     onStart: {
-        //         duration: 2000, // ms
-        //         render: function ( $container ) {
-        //             $container.addClass( 'slide-out' );
-        //         }
-        //     },
-        //     onAfter: function( $container ) {
-        //         $container.removeClass( 'slide-out' );
-        //     }
-        // };
+            onAfter: function( $container ) {
+                $container.removeClass( 'slide-out' );
+            }
+        };
  
-        // $( '#container' ).smoothState( settings );
+        $( '#container' ).smoothState( settings );
     } );
  
 })( jQuery );
